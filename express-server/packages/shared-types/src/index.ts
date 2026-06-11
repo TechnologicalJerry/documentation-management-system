2496400b83602e454e284869fee13632db6d0945
Rajat Verma <cserajatverma@gmail.com>
2026-06-11 10:09:27 +0530

    Add shared types export for authentication, user, project, document, common, and event types to enhance type organization and accessibility in the Express server.

---
+++ express-server/packages/shared-types/src/index.ts
@@ -0,0 +1,6 @@
+export * from './auth.types';
+export * from './user.types';
+export * from './project.types';
+export * from './document.types';
+export * from './common.types';
+export * from './events.types';
