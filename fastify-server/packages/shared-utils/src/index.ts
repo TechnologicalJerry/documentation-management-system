8e8679b389166fa00814d45828ea53a9f1622f57
Rajat Verma <cserajatverma@gmail.com>
2026-06-26 09:18:04 +0530

    feat: export utility functions from shared-utils package including logger, error handling, response, pagination, crypto, date, and validation helpers

---
+++ fastify-server/packages/shared-utils/src/index.ts
@@ -0,0 +1,7 @@
+export * from './logger';
+export * from './errors';
+export * from './response.helper';
+export * from './pagination.helper';
+export * from './crypto.helper';
+export * from './date.helper';
+export * from './validation.helper';
